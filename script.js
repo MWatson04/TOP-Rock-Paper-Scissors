b3c787e6c1261c6fc965df8e4d646c3fe9841b83
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-14 17:48:24 -0500

    Reworked two local arrays into one global array to get rid of too much repition

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-/* Used to determine when the human choice function can break */
 let validChoice = false;
+let humanScore = 0;
+let computerScore = 0;
+let totalRounds = 5;
+let gameOptions = ["rock", "paper", "scissors"];
 
 /* 
 Function that allows computer player to "choose" 
 between rock, paper, or scissors 
 */
 function getComputerChoice() {
-    /* Computer player options */
-    let comOptions = ["rock", "paper", "scissors"];
-    /* Generate random number */
+    // Generate random number
     comChoice = Math.floor(Math.random() * 100) + 1;
 
     switch (true) {
         case comChoice >= 1 && comChoice <= 33:
-            return comOptions[0];
+            return gameOptions[0];
             break;
         case comChoice >= 34 && comChoice <= 66:
-            return comOptions[1];
+            return gameOptions[1];
             break;
         case comChoice >= 67 && comChoice <= 100:
-            return comOptions[2];
+            return gameOptions[2];
             break;
     }
 }
@@ -29,23 +30,21 @@ Function that allows human player to make
 their choice of rock, paper, or scissors
 */
 function getHumanChoice() {
-    /* Human options to choose */
-    let humOptions = ["rock", "paper", "scissors"];
-    /* Prompt user to enter choice */
+    // Prompt user to enter choice
     let humChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
 
     switch (true) {
-        case humChoice === "rock":
+        case humChoice === gameOptions[0]:
             validChoice = true;
-            return humOptions[0];
+            return gameOptions[0];
             break;
-        case humChoice === "paper":
+        case humChoice === gameOptions[1]:
             validChoice = true;
-            return humOptions[1];
+            return gameOptions[1];
             break;
-        case humChoice === "scissors":
+        case humChoice === gameOptions[2]:
             validChoice = true;
-            return humOptions[2];
+            return gameOptions[2];
             break;
         default:
             console.log("Your choice was not valid. Please try again");
@@ -54,7 +53,17 @@ function getHumanChoice() {
     }
 }
 
-/* Gets code to run in console on web browser */
+/* function keepScore(humanValue, computerValue) {
+    humanValue = getHumanChoice();
+    computerValue = getComputerChoice();
+
+    Switch (true) {
+        case humanValue === "rock"
+    }
+
+} */
+
+// Gets code to run in console on web browser
 function run() {
     while (validChoice != true) {
         console.log("You choose: " + getHumanChoice());
