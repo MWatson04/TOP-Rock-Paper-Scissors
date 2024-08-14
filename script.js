44c109121c791b49e03b8297d7e3901d5b7228c6
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-14 14:42:22 -0500

    Coverted repeating if statements to switch statements for human and computer choice functions

--- script.js
+++ script.js
@@ -11,14 +11,16 @@ function getComputerChoice() {
     /* Generate random number */
     comChoice = Math.floor(Math.random() * 100) + 1;
 
-    if (comChoice >= 1 && comChoice <= 33) {
-        return comOptions[0];
-    }
-    else if (comChoice >= 34 && comChoice <= 66) {
-        return comOptions[1];
-    }
-    else if (comChoice >= 67 && comChoice <= 100) {
-        return comOptions[2];
+    switch (true) {
+        case comChoice >= 1 && comChoice <= 33:
+            return comOptions[0];
+            break;
+        case comChoice >= 34 && comChoice <= 66:
+            return comOptions[1];
+            break;
+        case comChoice >= 67 && comChoice <= 100:
+            return comOptions[2];
+            break;
     }
 }
 
@@ -27,25 +29,28 @@ Function that allows human player to make
 their choice of rock, paper, or scissors
 */
 function getHumanChoice() {
+    /* Human options to choose */
     let humOptions = ["rock", "paper", "scissors"];
-    
+    /* Prompt user to enter choice */
     let humChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
 
-    if (humChoice === "rock") {
-        validChoice = true;
-        return humOptions[0];
-    }
-    else if (humChoice === "paper") {
-        validChoice = true;
-        return humOptions[1];
-    }
-    else if (humChoice === "scissors") {
-        validChoice = true;
-        return humOptions[2];
-    }
-    else {
-        console.log("Your choice was not valid. Please try again");
-        validChoice = false;
+    switch (true) {
+        case humChoice === "rock":
+            validChoice = true;
+            return humOptions[0];
+            break;
+        case humChoice === "paper":
+            validChoice = true;
+            return humOptions[1];
+            break;
+        case humChoice === "scissors":
+            validChoice = true;
+            return humOptions[2];
+            break;
+        default:
+            console.log("Your choice was not valid. Please try again");
+            validChoice = false;
+            break;
     }
 }
 
