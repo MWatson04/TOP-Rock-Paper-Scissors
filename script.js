926b324272ad93c6bf7dccfad7ad47c911c4cc25
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-14 14:17:19 -0500

    Created function that allows user player to make their choice of rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* Global Variables */
+let validChoice = false;
+
 /* 
 Function that allows computer player to "choose" 
 between rock, paper, or scissors 
@@ -19,4 +22,35 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+/* 
+Function that allows human player to make
+their choice of rock, paper, or scissors
+*/
+function getHumanChoice() {
+    let humOptions = ["rock", "paper", "scissors"];
+    
+    let humChoice = prompt("Enter Rock, Paper, or Scissors: ").toLowerCase();
+
+    if (humChoice === "rock") {
+        validChoice = true;
+        return humOptions[0];
+    }
+    else if (humChoice === "paper") {
+        validChoice = true;
+        return humOptions[1];
+    }
+    else if (humChoice === "scissors") {
+        validChoice = true;
+        return humOptions[2];
+    }
+    else {
+        console.log("Your choice was not valid. Please try again");
+        validChoice = false;
+    }
+}
+
+while (validChoice != true) {
+    console.log("You choose: " + getHumanChoice());
+}
+
+console.log("Computer chooses: " + getComputerChoice());
