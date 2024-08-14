2cb25f3c0de762d5790a26942c7dd9387889144b
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-14 13:56:33 -0500

    Created functionality for computer player to make their choice in the rock, paper, scissors game

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Hello World")
+
+/* 
+Function that allows computer player to "choose" 
+between rock, paper, or scissors 
+*/
+function getComputerChoice() {
+    /* Computer player options */
+    let comOptions = ["rock", "paper", "scissors"];
+    /* Generate random number */
+    comChoice = Math.floor(Math.random() * 100) + 1;
+
+    if (comChoice >= 1 && comChoice <= 33) {
+        return comOptions[0];
+    }
+    else if (comChoice >= 34 && comChoice <= 66) {
+        return comOptions[1];
+    }
+    else if (comChoice >= 67 && comChoice <= 100) {
+        return comOptions[2];
+    }
+}
+
+console.log(getComputerChoice());
