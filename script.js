a2dcd31dce040b2f54835935a3706cacaa3cb953
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-08-14 19:47:18 -0500

    Added functionality to keep track of score of game

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 let validChoice = false;
 let humanScore = 0;
 let computerScore = 0;
-let totalRounds = 5;
+let roundsToWin = 3;
 let gameOptions = ["rock", "paper", "scissors"];
+let gameOver = false;
+let globalHumanChoice = "";
+let globalComputerChoice = "";
 
-/* 
-Function that allows computer player to "choose" 
-between rock, paper, or scissors 
-*/
-function getComputerChoice() {
-    // Generate random number
-    comChoice = Math.floor(Math.random() * 100) + 1;
-
-    switch (true) {
-        case comChoice >= 1 && comChoice <= 33:
-            return gameOptions[0];
-            break;
-        case comChoice >= 34 && comChoice <= 66:
-            return gameOptions[1];
-            break;
-        case comChoice >= 67 && comChoice <= 100:
-            return gameOptions[2];
-            break;
-    }
-}
 
 /* 
 Function that allows human player to make
@@ -35,17 +18,17 @@ function getHumanChoice() {
 
     switch (true) {
         case humChoice === gameOptions[0]:
+            globalHumanChoice = gameOptions[0];
             validChoice = true;
-            return gameOptions[0];
-            break;
+            return globalHumanChoice;
         case humChoice === gameOptions[1]:
+            globalHumanChoice = gameOptions[1];
             validChoice = true;
-            return gameOptions[1];
-            break;
+            return globalHumanChoice;
         case humChoice === gameOptions[2]:
+            globalHumanChoice = gameOptions[2];
             validChoice = true;
-            return gameOptions[2];
-            break;
+            return globalHumanChoice;
         default:
             console.log("Your choice was not valid. Please try again");
             validChoice = false;
@@ -53,23 +36,94 @@ function getHumanChoice() {
     }
 }
 
-/* function keepScore(humanValue, computerValue) {
-    humanValue = getHumanChoice();
-    computerValue = getComputerChoice();
 
-    Switch (true) {
-        case humanValue === "rock"
+/* 
+Function that allows computer player to "choose" 
+between rock, paper, or scissors 
+*/
+function getComputerChoice() {
+    validChoice = false;
+
+    // Generate random number
+    comChoice = Math.floor(Math.random() * 100) + 1;
+
+    switch (true) {
+        case comChoice >= 1 && comChoice <= 33:
+            globalComputerChoice = gameOptions[0];
+            return globalComputerChoice;
+        case comChoice >= 34 && comChoice <= 66:
+            globalComputerChoice = gameOptions[1];
+            return globalComputerChoice;
+        case comChoice >= 67 && comChoice <= 100:
+            globalComputerChoice = gameOptions[2];
+            return globalComputerChoice;
     }
+}
 
-} */
+function getScore(humanValue, computerValue) {
+    humanValue = globalHumanChoice;
+    computerValue = globalComputerChoice;
+
+    switch (true) {
+        case humanValue === gameOptions[0] && computerValue === gameOptions[0]:
+            console.log("This round is a tie!");
+            break;
+        case humanValue === gameOptions[0] && computerValue === gameOptions[1]:
+            console.log("Computer wins this round!");
+            computerScore++;
+            break;
+        case humanValue === gameOptions[0] && computerValue === gameOptions[2]:
+            console.log("You win this round!");
+            humanScore++;
+            break;
+        case humanValue === gameOptions[1] && computerValue === gameOptions[0]:
+            console.log("You win this round!");
+            humanScore++;
+            break;
+        case humanValue === gameOptions[1] && computerValue === gameOptions[1]:
+            console.log("This round is a tie!");
+            break;
+        case humanValue === gameOptions[1] && computerValue === gameOptions[2]:
+            console.log("Computer wins this round!");
+            computerScore++;
+            break;
+        case humanValue === gameOptions[2] && computerValue === gameOptions[0]:
+            console.log("Computer wins this round!");
+            computerScore++;
+            break;
+        case humanValue === gameOptions[2] && computerValue === gameOptions[1]:
+            console.log("You win this round!");
+            humanScore++;
+            break;
+        case humanValue === gameOptions[2] && computerValue === gameOptions[2]:
+            console.log("This round is a tie!");
+        default:
+            console.log("Error...Default Option Chosen")
+            break;
+    }
+}
 
 // Gets code to run in console on web browser
 function run() {
-    while (validChoice != true) {
+    while (!validChoice) {
         console.log("You choose: " + getHumanChoice());
     }
-    
+
     console.log("Computer chooses: " + getComputerChoice());
+    console.log(getScore(globalHumanChoice, globalComputerChoice));
 }
 
-run();
+function endGame() {
+    console.log("Game is OVER!!!");
+}
+
+if (humanScore != roundsToWin && computerScore != roundsToWin) {
+    while (!gameOver) {
+        run();
+    }
+}
+else if (humanScore === roundsToWin || computerScore === roundsToWin) {
+    gameOver = true;
+
+    endGame();
+}
